cb978a8033781c5703d84783512b795aeac5235f
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2023-02-25 12:50:45 +0200

    task 4

--- src/helpers/FormatDate.jsx
+++ src/helpers/FormatDate.jsx
@@ -1,3 +1,6 @@
-import { formatDistanceToNow } from 'date-fns';
+import { formatDistanceToNow, format } from 'date-fns';
+
 
 export const dateToNow = (date) => formatDistanceToNow(new Date(date), { addSuffix: true });
+
+export const formatDate = (data) => format(new Date(data), "Pp",{ addSuffix: true })
